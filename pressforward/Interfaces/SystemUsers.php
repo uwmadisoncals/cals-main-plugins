a80e3ed95f5e3c108dd89dfdda1668cddb5d5dcd
Al Nemec <al.nemec@gmail.com>
2016-11-21 10:36:18 -0600

    removing the pressforward plugin

--- pressforward/Interfaces/SystemUsers.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace PressForward\Interfaces;
-interface SystemUsers
-{
-	public function did_user_nominate($post_id, $user_id);
-	public function get_user_option( $option );
-	public function current_user_can( $capacity );
-	public function user_level( $option_name, $role );
-}
